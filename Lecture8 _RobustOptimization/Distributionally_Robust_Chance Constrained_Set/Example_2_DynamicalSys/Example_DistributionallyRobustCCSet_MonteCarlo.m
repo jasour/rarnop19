28247c8f61f3537c28eabc0a6f029601e0453e0b
Ashkan Jasour <ashkan.jasour@gmail.com>
2019-10-28 15:29:50 -0400

    Add files via upload

---
+++ Lecture8 _RobustOptimization/Distributionally_Robust_Chance Constrained_Set/Example_2_DynamicalSys/Example_DistributionallyRobustCCSet_MonteCarlo.m
@@ -0,0 +1,36 @@
+% MIT 16.S498: Risk Aware and Robust Nonlinear Planning, Fall 2019
+% Lecture 8: Nonlinear Robust Optimizaion
+%% Distributionally Robust Chance Constrained Set 
+%% Monte Carlo test
+
+Prob_List=[];
+for u=-1:0.01:1
+
+% number of uncertainty samples
+N=1000000;
+
+
+x1=random('Uniform',-0.1,0.1,1,N);
+x2=random('Uniform',-0.1,0.1,1,N);
+x3=random('Uniform',-0.1,0.1,1,N);
+
+m4=random('Uniform',-0.1,0.1,1,N);sigma4=random('Uniform',0.1,0.3,1,N);
+w=random('Normal',m4,sigma4);
+
+% x(k+1)
+f1=0.2*w.*x2;
+f2=x1.*x3;
+f3=1.2*x1-0.5*x2+x3+2*u;
+
+% success set
+K=-(1^2-(f1-0.1).^2/0.2^2-(f2-0.1).^2/0.2^2-(f3-0.4).^2/0.3^2);
+
+
+Pro=size(find(K>=0),2)/N;
+Prob_List=[Prob_List;u,Pro];
+
+end
+plot([-1:0.01:1],Prob_List(:,2),'--','LineWidth',3,'color','blue');hold on
+plot([-1:0.01:1],1-Delta*ones(size([-1:0.01:1])),'--','color','blue','LineWidth',5);grid;hold on
+str2 = '$ 1-\Delta $';text(0.6,0.9,str2,'Interpreter','latex','FontSize',30,'color','blue')         
+grid on; xlim([-0.9 0.9]); ylim([0 1.5]);
